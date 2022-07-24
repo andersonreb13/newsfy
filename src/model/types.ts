2320134fbc149a3b0c71d8d7f09aa178b437d75c
Anderson <andersonmatos11@gmail.com>
2022-07-23 23:05:25 -0300

    Add types for article object

---
+++ src/model/types.ts
@@ -0,0 +1,17 @@
+export interface Article {
+	id: number;
+	importDate: Date;
+	title: string[];
+	link: string[];
+	description: string[];
+	pubDate: Date[];
+	guid: string[];
+	"media:content": MediaContent[];
+}
+
+interface MediaContent {
+	$: {
+		width: number;
+		url: string;
+	};
+}
