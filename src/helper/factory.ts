238d4f5410261922a1b885b8a4d15068fd926cdc
Anderson <andersonmatos11@gmail.com>
2022-07-27 01:08:01 -0300

    Add factory to article

---
+++ src/helper/factory.ts
@@ -0,0 +1,21 @@
+import { Article } from "../model/types";
+
+const articleFactory = (list: any): Article[] => {
+	const articlesList = list.map((element: any) => {
+		let article = {} as Article;
+
+		article.title = element.title[0];
+		article.link = element.link[0];
+		article.description = element.description[0];
+		article.publicationDate = new Date(element.pubDate[0]);
+		article.externalId = element.guid[0]["_"] || element.guid[0];
+		article.mainPicture =
+			element["media:content"][1]?.["$"].url ||
+			element["media:content"][0]["$"].url;
+
+		return article;
+	});
+	return articlesList;
+};
+
+module.exports = { articleFactory };
