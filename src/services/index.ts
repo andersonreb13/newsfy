c88925360069f7fb98acbc29e7e32fcfb35921ea
Anderson <andersonmatos11@gmail.com>
2022-07-26 21:08:58 -0300

    Refactoring service layer to call updateArticle

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,4 +1,6 @@
-import { Article } from "../model/types";
+import { ExternalId } from "./../model/types";
+import { Raw, Article } from "../model/types";
+const { toArticle } = require("../helper/articleFactory");
 
 const axios = require("axios");
 const xml2js = require("xml2js");
@@ -9,17 +11,46 @@ const importData = async (url: string) => {
 		const { data } = await axios.get(url);
 		const { rss } = await xml2js.parseStringPromise(data);
 
-		if (!rss) return { data: null };
-
-		await repository.recordRssRaw(JSON.stringify(rss));
-		/** Antes de inserir os Artigos, fazer um select para um array artigosInseridos[].
-		 * Fazer uma lógica para que antes de inserir um novo artigo, verificar se o mesmo existe no array artigosInseridos[].
-		 * Caso EXISTA, cair no fluxo de UPDATE um Artigo já existente WHERE guid|externalId sejam iguais.
-		 * Caso NÃO EXISTA, cair no fluxo de INSERT de um novo Artigo.
-		 */
-		rss.channel[0].item.forEach((a: Article) => repository.addArticle(a));
+		if (!rss) return;
+		await saveRaw(rss);
+		await saveArticles(rss.channel[0].item);
 
 		return rss;
+	} catch (error) {
+		console.log({ error });
+		throw error;
+	}
+};
+
+const saveRaw = async (raw: Raw) => {
+	try {
+		await repository.recordRssRaw(JSON.stringify(raw));
+	} catch (error) {
+		console.log({ error });
+		throw error;
+	}
+};
+
+// Improvement method
+const saveArticles = async (articlesIncomingList: any[]) => {
+	try {
+		const currentExtIdArticles: ExternalId[] =
+			await repository.getAllExternalId();
+		const addedArticles: string[] = currentExtIdArticles.map(
+			(c: ExternalId) => c.externalId
+		);
+
+		articlesIncomingList.forEach((articleIncoming: any) => {
+			const article: Article = articleIncoming.guid[0].hasOwnProperty("_")
+				? toArticle(articleIncoming)
+				: articleIncoming;
+
+			if (addedArticles.includes(article.guid[0])) {
+				repository.updateArticle(article);
+			} else {
+				repository.addArticle(article);
+			}
+		});
 	} catch (error) {
 		console.log({ error });
 		throw error;
