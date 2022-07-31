e8eb2ffc5bc81b0412275df5ce270d2224b5999c
Anderson <andersonmatos11@gmail.com>
2022-07-31 12:01:37 -0300

    Refactoring factory

--- src/helper/factory.ts
+++ src/helper/factory.ts
@@ -1,13 +1,13 @@
 import { Article } from "../model/types";
 
-export const articleFactory = (list: any): Article[] => {
+export const articleFactory = (list: any[]): Article[] => {
 	const articlesList = list.map((element: any) => {
-		let article = {} as Article;
+		const article = {} as Article;
 
 		article.title = element.title[0];
 		article.link = element.link[0];
 		article.description = element.description[0];
-		article.publicationDate = new Date(element.pubDate[0]);
+		article.publicationDate = element.pubDate[0];
 		article.externalId = element.guid[0]["_"] || element.guid[0];
 		article.mainPicture =
 			element["media:content"][1]?.["$"].url ||
